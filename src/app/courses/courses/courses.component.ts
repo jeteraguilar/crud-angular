81ab435a38f43f9b35c110435c5e9ce5899a5f2f
Jeter Aguilar <jeteraguilar@gmail.com>
2022-08-08 17:17:45 -0300

    chamada Http Get no Angular e RXJS

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,6 +2,7 @@ import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -10,16 +11,14 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: "1", name: 'Angular', category: 'front-end'}
-  ];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
  // coursesService: CoursesService;
 
   constructor(private coursesService: CoursesService) {
-    // this.courses = [];
-    // this.coursesService = new CoursesService();
+    /*  this.courses =[];
+     this.coursesService = new CoursesService(); */
     this.courses = this.coursesService.list();
    }
 
