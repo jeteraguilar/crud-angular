2708c8b2c6ade26202727da0e32f375cf3e34973
Jeter Aguilar <jeteraguilar@gmail.com>
2022-08-09 15:31:56 -0300

    Lista de cursos, tratamento de erros e MatDialog

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,20 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { SharedModule } from '../shared/shared.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
 
 @NgModule({
-  declarations: [
-    CoursesComponent
-  ],
+  declarations: [CoursesComponent],
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    AppMaterialModule
-
-  ]
+    AppMaterialModule,
+    SharedModule,
+  ],
 })
-export class CoursesModule { }
+export class CoursesModule {}
